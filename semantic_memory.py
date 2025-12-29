ea05d4531138c334b9c206debe2dbce0cb2c6663
shappycc2010-ctrl <shappycc2010@gmail.com>
2025-12-29 21:12:59 +0100

    Create semantic_memory.py

---
+++ semantic_memory.py
@@ -0,0 +1,14 @@
+import json
+
+MEMORY_FILE = "semantic_memory.json"
+
+def load_memory():
+    try:
+        with open(MEMORY_FILE, "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+def save_memory(data):
+    with open(MEMORY_FILE, "w") as f:
+        json.dump(data, f, indent=4)
